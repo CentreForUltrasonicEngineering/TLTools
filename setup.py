5214baf53e65523180fef585324a3a812b9d6f3c
Jerzy Dziewierz @biurko <jerzy.dziewierz@strath.ac.uk>
2017-11-15 06:18:28 +0000

    updating the setup.py. Somehow I was not on the author list.

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup, find_packages
 import os
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(name='TLTools',
-      version='2.0.5',
+      version='2.1.5',
       description='Tools for acquiring and processing Ultrasonic NDT data',
-      author='Timothy Lardner',
-      author_email='timlardner@gmail.com',
-      url='https://github.com/timlardner',
-      license = "Not to be used by anyone",
+      author='Timothy Lardner, Jerzy Dziewierz',
+      author_email='jerzy.dziewierz@strath.ac.uk',
+      url='https://github.com/jerzydziewierz',
+      license="Not to be used by anyone",
       packages=['TLTools'],
       install_requires=[
-          'pycuda','matplotlib','numpy','viscm','scipy','pillow','pythonnet',
+          'pycuda', 'matplotlib', 'numpy', 'viscm', 'scipy', 'pillow', 'pythonnet',
       ],
-      long_description='This is a test of TFM awesomeness',
+      long_description='cueTFM part of the cueART',
       package_data={'': ['TFMKernel.cu']},
       classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: Other/Proprietary License",
         "Intended Audience :: Developers",
